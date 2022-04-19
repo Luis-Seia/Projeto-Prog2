abf4956cddd7f98b72e5fb4e56cdcde3d73faf11
Luis-Seia <luis.seiax@gmail.com>
2022-04-19 09:31:23 +0200

    Criei a classe pessoa

---
+++ Projeto-Prog2/src/Utilizador/Pessoa.java
@@ -0,0 +1,63 @@
+package Utilizador;
+
+public abstract class Pessoa {
+	private String nome;
+	private int idade;
+	private char genero;
+	private String contacto;
+	private String email;
+	private String endereco;
+	private String NumeroBI;
+	private String dataNascimento;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public char getGenero() {
+		return genero;
+	}
+	public void setGenero(char genero) {
+		this.genero = genero;
+	}
+	public String getContacto() {
+		return contacto;
+	}
+	public void setContacto(String contacto) {
+		this.contacto = contacto;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getNumeroBI() {
+		return NumeroBI;
+	}
+	public void setNumeroBI(String numeroBI) {
+		NumeroBI = numeroBI;
+	}
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+	
+}
